6ef852d5f73276ac3f72664980567db83566188d
Rucciva <rucciva@gmail.com>
2017-12-28 21:10:21 +0700

    auto detect plugin from rockspec

--- assets/install-plugins.sh
+++ assets/install-plugins.sh
@@ -2,27 +2,18 @@
 
 set -e
 
-if [ -n "$KONG_DEV_PLUGIN_NAME"  ]; then
-    # install single plugins
-    cd ${KONG_TEMP_PLUGIN_DIRECTORY}/${KONG_DEV_PLUGIN_NAME}
-    luarocks make
-    installed=$KONG_DEV_PLUGIN_NAME
-
-else
-    # install multiple plugins
-    # each plugins should be contained under the folder named with it's name
-    cd ${KONG_TEMP_PLUGIN_DIRECTORY}
-    installed=""
-    for i in $(ls -d */); do 
-        cd ${KONG_TEMP_PLUGIN_DIRECTORY}/$i
-        luarocks make
-        
-        if [ -n "$installed" ]; then
-            installed="${installed},"
-        fi
-        installed="${installed}${i%%/}"
-    done
-fi
+cd $KONG_TEMP_PLUGIN_DIRECTORY
+installed="mobdebug"
+for f in `ls | grep rockspec`; do 
+    if [ -f $f ]; then 
+        PLUGIN_NAME=`echo $f | awk -F'-' '{print $3}'`
+        echo "installing $PLUGIN_NAME"
+        luarocks make $f   
+        installed="${installed},${PLUGIN_NAME}"
+    fi;
+done
 
 # record installed plugin for later use
-echo "mobdebug,${installed}" > $KONG_INSTALLED_CUSTOM_PLUGINS_LIST
+echo "Installed Custom Plugins : $installed"
+echo $installed >> $KONG_INSTALLED_CUSTOM_PLUGINS_LIST
+
